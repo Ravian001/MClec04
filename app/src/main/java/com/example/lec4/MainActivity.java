b92337cbb3fbd909975a9c6a0d35e73c080f4764
Hassan001 <bitf19a001@pucit.edu.pk>
2022-10-30 15:49:24 +0500

    buttonsetonclick

--- app/src/main/java/com/example/lec4/MainActivity.java
+++ app/src/main/java/com/example/lec4/MainActivity.java
@@ -7,7 +7,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-/*public class MainActivity extends AppCompatActivity {
+
+
+public class MainActivity extends AppCompatActivity {
 TextView textView;
 Button button;
 
@@ -15,9 +17,9 @@ Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);*/
+        setContentView(R.layout.activity_main);
 
-/*        textView= findViewById(R.id.hassantext);
+        textView= findViewById(R.id.hassantext);
         button = findViewById(R.id.hassanbutton);
 
         button.setOnClickListener(new View.OnClickListener() {
@@ -26,63 +28,7 @@ Button button;
                 textView.setText("Text is changed by setOnClickListener");
             }
 
-        });*/
-
-
-
-/*
-    }
-
-
-}*/
-
-
-
-
-
-
-
-
-
-
-
-
-    public class MainActivity extends AppCompatActivity implements View.OnClickListener
-    {
-
-        TextView textView;
-        Button button;
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-
-        textView= findViewById(R.id.hassantext);
-        button = findViewById(R.id.hassanbutton);
-            button.setOnClickListener(this);
-            textView.setOnClickListener(this);
-        }
-        @Override
-        public void onClick(View view)
-        {
-            switch (view.getId())
-            {
-                //handle multiple view click events
-                case R.id.hassantext:
-                    textView.setText("changed by my listener");
-                    break;
-                case R.id.hassanbutton:
-                    textView.setText("Button");
-                    break;
-                default:
-                    textView.setText("Nothing");
-                    break;
-            }
-
-
-
-
-        }
+        });
 
 
 
@@ -90,53 +36,13 @@ Button button;
     }
 
 
-
-
-
-/*
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener
-{
-
-
-            TextView textView;
-        Button button;
-
-    @Override
-    public void onClick(View view) {
-
-    }
-
-    private class ClickListener implements View.OnClickListener
-    {
-
-
-
-
-        @Override
-        public void onClick(View view)
-        {
-            switch (view.getId())
-            {
-                //handle multiple view click events
-
-                                case R.id.hassantext:
-                                textView.setText("TextView");
-                                break;
-                case R.id.hassanbutton:
-                    textView.setText("Button");
-                    break;
-                default:
-                    textView.setText("Nothing");
-                    break;
-            }
-
-            textView =findViewById(R.id.hassantext);
-            button = findViewById(R.id.hassanbutton);
-            button.setOnClickListener(this);
-            textView.setOnClickListener(this);
-        }
-    }
 }
 
-*/
+
+
+
+
+
+
+
+
